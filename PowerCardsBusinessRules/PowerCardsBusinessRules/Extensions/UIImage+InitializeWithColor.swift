e575555370563e566f9fa48409c1e2706d9a881b
Peter Ivanics <peter@choicely.com>
2018-06-02 09:14:11 +0200

    RefactoringAdded UIImage+InitializeWithColor

---
+++ PowerCardsBusinessRules/PowerCardsBusinessRules/Extensions/UIImage+InitializeWithColor.swift
@@ -0,0 +1,23 @@
+//
+//  UIImage+InitializeWithColor.swift
+//  PowerCardsBusinessRules
+//
+//  Created by Peter Ivanics on 31/05/2018.
+//  Copyright © 2018 Lovented Oy. All rights reserved.
+//
+
+extension UIImage {
+    convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        
+        color.setFill()
+        UIRectFill(rect)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage)
+    }
+}
