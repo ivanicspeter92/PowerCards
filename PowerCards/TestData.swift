2b42c3ac15d5239619b9db234c39f208b6eaaee7
Peter Ivanics <peter@choicely.com>
2018-06-06 21:05:28 +0200

    Added test data

--- PowerCards/TestData.swift
+++ PowerCards/TestData.swift
@@ -9,22 +9,24 @@
 import PowerCardsBusinessRules
 
 struct TestData {
+    static var endocrineCards: [Powercard] {
+        return [
+            PowerFlashCard(name: "Hypothalamus", subTitle: "Eleifend ligula lorem turpis consectetuer, fusce integer eget ultricies inceptos.", image: UIImage(named: "Hypothalamus")),
+            PowerFlashCard(name: "Pineal body", subTitle: "Eleifend ligula lorem turpis consectetuer, fusce integer eget ultricies inceptos.", image: UIImage(named: "Pineal body")),
+            PowerFlashCard(name: "Digestive system", subTitle: "Eleifend ligula lorem turpis consectetuer, fusce integer eget ultricies inceptos.", image: UIImage(named: "Digestive system")),
+            PowerFlashCard(name: "Kidney", subTitle: nil, image: UIImage(named: "Kidney"))
+        ]
+    }
+    
     static var testDeckSet: Set<Powerdeck> {
         return [
-            Powerdeck(id: "1", name: "Test", creator: testUser),
-            Powerdeck(id: "2", name: "Test deck 2", creator: testUser, isShared: true)
+//            Powerdeck(id: "1", name: "Test", creator: testUser),
+//            Powerdeck(id: "2", name: "Test deck 2", creator: testUser, isShared: true),
+            Powerdeck(id: UUID().uuidString, name: "Endocrine", creator: testUser, cards: endocrineCards)
         ]
     }
     
     static var testUser: User {
         return User(id: 1, name: "Peter Ivanics", email: "peter.ivanics@helsinki.fi")
     }
-//    [
-//    Powercard(id: "1", name: "bla"),
-//    Powercard(id: "2", name: "blabla")
-//    ]
-//    [
-//    Powercard(id: "3", name: "BLA", subTitle: "My favorite deck"),
-//    Powercard(id: "4", name: "BLABLA")
-//    ]
 }
